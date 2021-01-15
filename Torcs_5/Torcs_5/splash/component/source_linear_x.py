48f5c73c6c1bb1ab0086374a73bf53101dee4ffe
cheonghwa-lee <hayaclee@snu.ac.kr>
2021-01-15 16:43:33 +0900

    tlqkf 2021.01.15

---
+++ Torcs_5/Torcs_5/splash/component/source_linear_x.py
@@ -0,0 +1,33 @@
+'''
+    Generated automatically by Splash Code Generator for source_linear_x
+'''
+from std_msgs.msg import Float32
+from geometry_msgs.msg import TwistStamped
+
+from scl.components import *
+from scl.impl.singleton import Singleton
+
+from ..factory.factory_0 import Factory0
+
+import time #haya
+
+class SourceLinearX(Component, metaclass=Singleton):
+    def __init__(self):
+        super().__init__(name="source_linear_x", factory=Factory0(), mode="Mode A")
+        self.priority = 1
+
+    def setup(self):
+        self.create_subscription(TwistStamped, "/torcs_speed", self.torcs_speed_callback, 1)
+        self.attach_stream_output_port(msg_type=Float32, channel="source_x")
+    
+    def run(self):
+        pass
+    
+    def torcs_speed_callback(self, msg):
+        linear_x = msg.twist.linear.x
+        new_msg = Float32()
+        new_msg.data = linear_x
+        # time.sleep(5)  
+        self.write("source_x", new_msg)
+    
+
